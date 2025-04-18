134e8d3ba5195f0bad07999e501892e10eab47d9
thehando0714 <chrishu0714@gmail.com>
2025-04-18 19:00:07 +0800

    sort

---
+++ CP-Program/Atcoder/atcoder202.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int a,b,c,sum;
+int main(){
+    cin>>a>>b>>c;
+    sum=sum+7-a;
+    sum=sum+7-b;
+    sum=sum+7-c;
+    cout<<sum<<endl;
+    return 0;
+}
